af0fd52843b74fed63f4b929202a610adf460bd0
pavel.kuznetsov.5x <pavel.kuznetsov.5x@gmail.com>
2020-06-14 17:24:38 +0300

    Initial

---
+++ app/src/main/java/com/spqrta/chatapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.spqrta.chatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import com.spqrta.chatapp.screens.login.LoginFragmentDirections
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findNavController(R.id.nav_host).navigate(LoginFragmentDirections.actionLoginFragmentToChatsFragment())
+    }
+}
---
+++ app/src/main/java/com/spqrta/chatapp/screens/login/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.spqrta.chatapp.screens.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+
+import com.spqrta.chatapp.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bLogin.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToChatsFragment())
+        }
+    }
+}
